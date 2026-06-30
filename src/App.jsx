4ac57eb8bce0cd3bbd3e12e88312e77a832ed653
Deepak kumar sharma <deepak4996sharma@gmail.com>
2026-06-30 21:24:46 +0530

    fix: updated import path for Services

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Products from "./Products/Products";
 {
   /* Cleanly imported your 3-image showcase file */
 }
-import Services from "./Services/Services";
+import Services from "./Services/service";
 {
   /* Cleanly imported your tabbed capabilities dashboard */
 }
